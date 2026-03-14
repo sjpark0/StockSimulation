b4e1537215c60ebeb88a2448bbc758f4d1107b8c
sjpark <sjpark0@gmail.com>
2026-03-14 20:21:41 +0900

    수정

--- back_test/src/main.rs
+++ back_test/src/main.rs
@@ -2,47 +2,73 @@ use std::env;
 mod stock_file;
 mod portfolio;
 
-use portfolio::Portfolio;
+use portfolio::Backtester;
+fn maimum_profit(capital_profit_vec : &[Option<(f64, f64, f64)>]) -> (f64, f64, f64){
+    let (mut max_capital, mut max_profit, mut max_mdd) = (0.0, 0.0, 0.0);
+    
+    for v in capital_profit_vec.iter(){
+        if let Some((c, p, m)) = v{
+            if max_capital < *c{
+                (max_capital, max_profit, max_mdd) = (*c, *p, *m);
+            }
+        }
+    }
 
+    (max_capital, max_profit, max_mdd)
+    
+}
+fn analyze(capital_profit_vec : &[Option<(f64, f64, f64)>], initial_price : f64) -> (f64, f64, u32, f64){
+    let mut cnt = 0.0;
+    for v in capital_profit_vec.iter(){
+        if v.is_some(){
+            cnt += 1.0;
+        }
+    }
+    let mut avg_capital = 0.0;
+    for v in capital_profit_vec.iter(){
+        if let Some((c, _, _)) = v{
+            avg_capital += c / cnt;
+        }
+    }
+    let mut std_capital = 0.0;
+    for v in capital_profit_vec.iter(){
+        if let Some((c, _, _)) = v{
+            std_capital += (avg_capital - c) * (avg_capital - c) / cnt;
+        }
+    }
+    
+    let mut num_win = 0;
+    for v in capital_profit_vec.iter(){
+        if let Some((c, _, _)) = v{
+            if *c > initial_price{
+                num_win += 1;
+            }
+        }
+    }
+    
+    (avg_capital, std_capital.sqrt(), num_win, (num_win as f64) / (cnt as f64))
+}
 fn main(){
     let args: Vec<String> = env::args().collect();
     let file_name = format!("{}.xlsx", args[1]);
     let prices = stock_file::load_excel_file(&file_name);
-    
+    let fee_rate = 0.25;
     let initial_capital = 100_000.0;
     let threshold = vec![0.1, 0.2, 0.3, 0.4, 0.5];
+    let vec_duration: Vec<usize> = vec![21, 60, 200, 400];
     
-    let initial_price = prices[0];
-    let initial_qty = (initial_capital / initial_price).round();
-    let initial_cash = initial_capital - initial_price * initial_qty;
-
-    let mut backtester: Vec<Portfolio> = vec![];
-    for t in &threshold{
-        backtester.push(Portfolio::new(initial_capital, initial_price, *t));
-    }
-
-    let mut val_vec = vec![0.0;threshold.len() + 2];
-    let mut acc_win = vec![0;threshold.len() + 2];
-    
-    for current_price in prices.iter(){
-        let buy_and_hold_value = initial_qty * current_price + initial_cash;
-        
-        val_vec[threshold.len()] = buy_and_hold_value;
-        val_vec[threshold.len() + 1] = initial_capital;
-        
-        for (j, tester) in backtester.iter_mut().enumerate(){
-            tester.process_price(*current_price);
-            let current_value = tester.get_total_value(*current_price);
-            val_vec[j] = current_value;
+    for t in threshold.iter(){
+        for duration in vec_duration.iter(){
+            let tmp_rolling = t.rolling_return(&prices, initial_capital, 0.5, *duration, fee_rate);
+            let (avg, std, num_win, perc) = analyze(&tmp_rolling, initial_capital);
+            let (max_cap, max_profit, max_mdd) = maimum_profit(&tmp_rolling);
+            println!("리밸런싱({}), 기간 {} : 평균 = {:.2}, 평균수익률 = {:.2}, 표준편차 = {:.2}, 플러스수익횟수 = {}, 플러스수익확률 = {:.2}, 최대수익금 = {:.2}, 최대수익률 = {:.2}, 최대수익때mdd = {:.2}", t, duration, avg, avg / initial_capital, std, num_win, perc, max_cap, max_profit, max_mdd);
         }
-
-        let (idx, max_val) = val_vec.iter().enumerate().fold((threshold.len() + 1, val_vec[threshold.len() + 1]), |(acc_id, acc_val), (id, &val)| if val > acc_val { (id, val)} else {(acc_id, acc_val)});
-        acc_win[idx] += 1;
     }
-    for idx in 0..threshold.len(){
-        println!("리밸런싱({})자산 : {}", threshold[idx] * 100.0, acc_win[idx]);
+    for duration in vec_duration.iter(){
+        let tmp_rolling = &(1.0).rolling_return(&prices, initial_capital, 1.0, *duration, fee_rate);
+        let (avg, std, num_win, perc) = analyze(&tmp_rolling, initial_capital);
+        let (max_cap, max_profit, max_mdd) = maimum_profit(&tmp_rolling);
+        println!("보유, 기간 {} : 평균 = {:.2}, 평균수익률 = {:.2}, 표준편차 = {:.2}, 플러스수익횟수 = {}, 플러스수익확률 = {:.2}, 최대수익금 = {:.2}, 최대수익률 = {:.2}, 최대수익때mdd = {:.2}", duration, avg, avg / initial_capital, std, num_win, perc, max_cap, max_profit, max_mdd);
     }
-    println!("단순보유 : {}", acc_win[threshold.len()]);
-    println!("현금 : {}", acc_win[threshold.len()+1]);
-    
 }
--- back_test/src/portfolio.rs
+++ back_test/src/portfolio.rs
@@ -1,16 +1,22 @@
+use crate::portfolio;
+
 pub struct Portfolio{
     cash: f64,
     stock_qty: u32,
     threshold: f64,
+    ratio : f64,
+    fee_rate : f64,
 }
 
 impl Portfolio{
-    pub fn new(initial_capital: f64, initial_price: f64, threshold: f64) -> Self{
-        let half_capital = initial_capital / 2.0;
-        let qty = (half_capital / initial_price).round() as u32;
-        let cash = initial_capital - (qty as f64) * initial_price;
-        Self { cash: cash, stock_qty: qty, threshold: threshold}
+    pub fn new(initial_capital: f64, initial_price: f64, threshold: f64, ratio: f64, fee_rate: f64) -> Self{
+        let tmp_stock_value = initial_capital * ratio;
+        let qty = (tmp_stock_value / initial_price).round() as u32;
+        let cash = initial_capital - (qty as f64) * initial_price * (1.0 + fee_rate * 0.01);
+
+        Self { cash: cash, stock_qty: qty, threshold: threshold, ratio : ratio, fee_rate: fee_rate}
     }
+    
     pub fn process_price(&mut self, current_price: f64){
         let stock_value = (self.stock_qty as f64) * current_price;
         let total_value = stock_value + self.cash;
@@ -23,13 +29,16 @@ impl Portfolio{
         }
     }
     pub fn rebalance(&mut self, current_price: f64, total_value: f64){
-        let half_capital = total_value / 2.0;
-        self.stock_qty = (half_capital / current_price).round() as u32;
-        self.cash = total_value - (self.stock_qty as f64) * current_price;
+        let tmp_stock_value = total_value * self.ratio;
+        let tmp_qty = (tmp_stock_value / current_price).round() as u32;
+        let fee = ((self.stock_qty as f64) - (tmp_qty as f64)).abs() * self.fee_rate * 0.01;
+        self.stock_qty = tmp_qty;
+        self.cash = total_value - (self.stock_qty as f64) * current_price - fee;
     }
     pub fn get_total_value(&self, current_price: f64) -> f64{
         self.cash + (self.stock_qty as f64) * current_price
     }
+    
 }
 
 pub trait Rate {
@@ -38,5 +47,37 @@ pub trait Rate {
 impl Rate for f64{
     fn get_profit_rate(&self, initial_value : f64) -> f64{
         (*self / initial_value) - 1.0
+    }
+}
+
+pub trait Backtester{
+    fn process_backtester(&self, price_history : &[f64], initial_capital : f64, start : usize, end : usize, stock_ratio : f64, fee_rate : f64) -> (f64, f64);
+    fn rolling_return(&self, price_history : &[f64], initial_capital : f64, stock_ratio : f64, duration : usize, fee_rate : f64) -> Vec<Option<(f64, f64, f64)>>;
+}
+impl Backtester for f64{
+    fn process_backtester(&self, price_history : &[f64], initial_capital : f64, start : usize, end : usize, stock_ratio : f64, fee_rate : f64) -> (f64, f64){
+        let mut portfolio = Portfolio::new(initial_capital, price_history[start], *self, stock_ratio, fee_rate);
+        for i in start..=end{
+            portfolio.process_price(price_history[i]);
+        }
+        let final_capital = portfolio.get_total_value(price_history[end]);   
+        let profit = final_capital.get_profit_rate(initial_capital);
+        (final_capital, profit)
+    }
+    fn rolling_return(&self, price_history : &[f64], initial_capital : f64, stock_ratio : f64, duration : usize, fee_rate : f64) -> Vec<Option<(f64, f64, f64)>>{
+        let mut res_vec: Vec<Option<(f64, f64, f64)>> = Vec::new();
+        
+        let mut prev_maximum: f64 = 0.0;
+        for i in 0..price_history.len(){
+            if i < duration{
+                res_vec.push(None);
+            }
+            else{
+                prev_maximum = prev_maximum.max(price_history[i]);
+                let (cap, profit) = self.process_backtester(price_history, initial_capital, i - duration, i, stock_ratio, fee_rate);
+                res_vec.push(Some((cap, profit, price_history[i] / prev_maximum)));
+            }
+        }
+        res_vec
     }
 }
